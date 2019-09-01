55b06fddec67bb381c83a29a3125c941e832b860
alfredo.salzillo <noctisbels@gmai.com>
2019-09-01 16:15:33 +0200

    fix ) at the end of the code

--- src/App/CarbonCode/CarbonCode.js
+++ src/App/CarbonCode/CarbonCode.js
@@ -5,7 +5,7 @@ const purifyCode = code => encodeURI(code).replace(/&/ig, '%26');
 const carbonEmbedUrl = 'https://carbon.now.sh/embed';
 const CarbonCode = ({ code, language, onLoad }) => code && html`
    <iframe
-    src="${carbonEmbedUrl}/?bg=rgba(64%252C64%252C64%252C0)&t=dracula&wt=none&wc=true&wa=false&ds=false&l=${language}&fl=1&fm=Hack&fs=14px&es=2x&wm=false&code=${purifyCode(code)})"
+    src="${carbonEmbedUrl}/?bg=rgba(64%252C64%252C64%252C0)&t=dracula&wt=none&wc=true&wa=false&ds=false&l=${language}&fl=1&fm=Hack&fs=14px&es=2x&wm=false&code=${purifyCode(code)}"
     @load=${onLoad}
     width="100%"
     height="500px"
