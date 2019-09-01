f68a4339308f3bd01e0883604c9485e44e2a5c3b
alfredo.salzillo <noctisbels@gmai.com>
2019-09-01 17:14:18 +0200

    code refactor

--- src/App/CarbonCode/CarbonCode.js
+++ src/App/CarbonCode/CarbonCode.js
@@ -1,12 +1,12 @@
 import { html } from 'lit-html';
 import { component } from 'haunted';
 
-const purifyCode = code => encodeURI(code).replace(/&/ig, '%26');
 const carbonEmbedUrl = 'https://carbon.now.sh/embed';
-function CarbonCode({ code, language, onLoad }) {
-  return code && html`
+
+const encodeCode = code => encodeURI(code).replace(/&/ig, '%26');
+const CarbonCode = ({ code, language, onLoad }) => code && html`
    <iframe
-    src="${carbonEmbedUrl}/?bg=rgba(64%252C64%252C64%252C0)&t=dracula&wt=none&wc=true&wa=false&ds=false&l=${language}&fl=1&fm=Hack&fs=14px&es=2x&wm=false&code=${purifyCode(code)}"
+    src="${carbonEmbedUrl}/?bg=rgba(64%252C64%252C64%252C0)&t=dracula&wt=none&wc=true&wa=false&ds=false&l=${language}&fl=1&fm=Hack&fs=14px&es=2x&wm=false&code=${encodeCode(code)}"
     @load=${onLoad}
     width="100%"
     .height=${code.split('\n').length * 35}
@@ -14,7 +14,6 @@ function CarbonCode({ code, language, onLoad }) {
     sandbox="allow-scripts allow-same-origin">
   </iframe>
   `;
-};
 CarbonCode.observedAttributes = ['code', 'language', 'onLoad'];
 
 export default component(CarbonCode);
